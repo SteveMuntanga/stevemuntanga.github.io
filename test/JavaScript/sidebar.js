ca0f63fe574cb59cdef85abc1272ee0aeb72338e
Steve Wina Muntanga <132919239+SteveMuntanga@users.noreply.github.com>
2023-08-02 14:31:45 +0200

    Delete test directory
    Deleted a directory

--- test/JavaScript/sidebar.js
+++
@@ -1,8 +0,0 @@
-﻿/* side bar open/close script */
-function wnpls_open() {
-  document.getElementById("sidebar").style.display = "block";
-}
-
-function wnpls_close() {
-  document.getElementById("sidebar").style.display = "none";
-}

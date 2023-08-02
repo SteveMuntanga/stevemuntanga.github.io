6be622aa2cec9c30c7499f6d46808b66b6aebb14
Steve Wina Muntanga <132919239+SteveMuntanga@users.noreply.github.com>
2023-08-02 14:14:48 +0200

    add files via upload
    uploaded some shitty files

---
+++ test/JavaScript/sidebar.js
@@ -0,0 +1,8 @@
+﻿/* side bar open/close script */
+function wnpls_open() {
+  document.getElementById("sidebar").style.display = "block";
+}
+
+function wnpls_close() {
+  document.getElementById("sidebar").style.display = "none";
+}
